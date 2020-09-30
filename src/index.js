9997d2684645f82140f01fb9f9779311a5a26d5b
Caio Rodrigues <rfcaio90@gmail.com>
2020-09-30 19:40:11 -0300

    add validation for create product route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator')
 
 const server = express()
 
 server.use(express.json())
 
-server.post('/product', (req, res) => {
-  const { name, price } = req.body
-  return res.status(201).json({
-    message: `Product ${name} with price ${price} created with success.`
-  })
-})
+server.post(
+  '/product',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage('Product name should not be empty.')
+      .isLength({ min: 6 })
+      .withMessage('Product name must have at least 6 characters.'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('You must provide a valid price.')
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, price } = req.body
+    return res.status(201).json({
+      message: `Product ${name} with price ${price} created with success.`
+    })
+  }
+)
 
 server.listen(3000)
