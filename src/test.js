37723f6af8576b463f37a0cce15f94e6851b6945
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-19 18:12:04 -0300

    add tests to list all products endpoint

--- src/test.js
+++ src/test.js
@@ -31,6 +31,24 @@ describe('product test suites', () => {
 
   afterEach(truncateProductDatabase)
 
+  describe('/product GET', () => {
+    beforeEach(insertDefaultProduct)
+
+    test('list all products successfully', async () => {
+      const response = await request(server).get('/product')
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual({
+        products: [
+          {
+            id: '5e392019-3d9a-463f-a5cd-a7e7e631be1c',
+            name: 'Arroz Alteza 1Kg',
+            price: 6.49
+          }
+        ]
+      })
+    })
+  })
+
   describe('/product/:id GET', () => {
     beforeEach(insertDefaultProduct)
 
